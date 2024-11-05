00750a6312e4f38244ddb5f25d90efe31008bcdb
andre_linux <andre_linux@Andreas.>
2024-11-05 16:24:37 +0100

    Simple Layout

---
+++ script.js
@@ -0,0 +1,79 @@
+// // Buttons Sandbox
+// // ContactMe / Download Resume / Availability Calendar 
+// //  Portfolio or Project / Language Skill 
+// // Social Media/ Linkedin / Toggle change language
+
+
+// // Header Section
+
+// // Language Toggle Button
+// // Top right corner ("EN | PT")
+// const popover = document.getElementById('mypopover');
+
+// popover.addEventListener('toggle', () => {
+
+// });
+// ontoggle = () => {};
+
+// // Key Interactive Buttons
+
+// // ###########################################################
+// // Contact Me Button
+// const contactMe = document.getElementById('contact');
+
+// contactMe.addEventListener('click', () => {
+//         open("./contactPage.html")
+// });
+
+// // ###############################################################
+// // Dowload Resume button
+
+// // First giving Permsision 
+
+// // Setting download API 
+
+// // Getting the HTML to PDF conversion 
+// // const button_download = document.getElementById('download');
+// // // let downloading = browser.downloads.download(
+// // //     Option
+// // // )
+
+// // button_download.addEventListener('click', () => {
+// //            open("./resume.html") 
+// // });
+
+// We will us  POPOVER API to render each button
+// #############################################
+// Goal button 
+
+// // #######################################################################
+// // Availability Calendar
+// const button_calendar = document.getElementById('calendar');
+// // Making Own custom calendar for a visible clear indications
+// // Very low logic use of js for this one.
+// button_calendar.addEventListener('click', () => {
+// //    Trigger My calendar to appear and show my availabilty
+// });
+
+// // ###########################################################
+// // Projects BUTTON 
+// // It would show Images that are screenshot of each 
+// // main page of different projects
+
+
+
+// // #########################################################
+// // Language Skills progress button
+// // Icon/Bars 
+
+// // ##########################################################
+// // Social Media / Linkdin 
+// // Just simple link html a tag
+
+// // ############################################################
+
+
+
+// // ##########################################################
+// // Skill transferable 
+// // Displaying minifying picture that point out to symbolic Skills
